0dbf224398e066f5903c1d3f959bb43b2c8062fc
markerviktor <MarkerViktor@outlook.com>
2020-08-20 03:50:20 +0600

    Remove mistakes from README.md and change entities set

--- room_access/entities.py
+++ room_access/entities.py
@@ -6,43 +6,34 @@ from room_access.app import db
 
 
 class User(db.Entity):
+    """Пользователь системы контроля доступа"""
     id = PrimaryKey(int, auto=True)
-    first_name = Required(str, 75)  # Имя
-    last_name = Optional(str, 75)  # Фамилия
-    visits = Set('Visit')
-    allowed_rooms = Set('Room')
-    groups = Set('Group')
+    first_name = Required(str, 75)  # Имя пользователя
+    last_name = Optional(str, 75)  # Фамилия пользователя
+    visits = Set('Visit')  # Посещения пользователя
+    allowed_rooms = Set('Room')  # Помещения, в которые разрешен доступ пользователю
 
-class Admin(User):
-    telegram_id = Required(str)
-    managed_rooms = Set('Room')
-
-class Student(User):
-    study_group = Required('StudyGroup')
-
-class Group(db.Entity):
-    name = PrimaryKey(str)
-    users = Set('User')
-    allowed_rooms = Set('Room')
-
-class StudyGroup(Group):
-    students = Set('Student')
-    institute = Optional(str)
-    department = Optional(str)
-    course_number = Optional(str)
 
 class Room(db.Entity):
-    location = PrimaryKey(str)
-    visits = Set('Visit')
-    allowed_users = Set('User')
-    allowed_user_groups = Set('Group')
-    admins = Set('Admin')
+    """Помещение, в котором контролируется доступ"""
+    location = PrimaryKey(str)  # Местоположение помещения
+    visits = Set('Visit')  # Посещения помещения
+    allowed_users = Set(User)  # Пользователи, которым разрешен доступ к помещению
+    admins = Set('Admin')  # Администраторы, имеющие право управлять доступом в данном помещении
+
 
 class Visit(db.Entity):
+    """Посещение пользователем помещения в конкретное время"""
     id = PrimaryKey(int, auto=True)
-    datetime = Required(datetime)
-    visited_room = Required('Room')
-    visitor = Required('User')
+    datetime = Required(datetime)  # Время посещения
+    visited_room = Required(Room)  # Помещение, которое посетили
+    visitor = Required(User)  # Посетитель
+
+
+class Admin(db.Entity):
+    """Администратор помещений"""
+    telegram_username = Required(str)  # имя пользователя администратора в телеграм
+    managed_rooms = Set(Room)  # Помещения, доступом в которые имеет право управлять данный администратор
 
 
 
