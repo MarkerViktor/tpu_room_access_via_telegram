fa4d10a41c7b9c73da3ea7b52a94176d4952c90f
markerviktor <MarkerViktor@outlook.com>
2020-08-23 16:59:27 +0600

    Добавлены функции get_admin_by_id и get_admin_by_username. Функция check_admin_username_exist заменена на check_admin_exist_by_username

--- room_access/services/admin_sevice.py
+++ room_access/services/admin_sevice.py
@@ -1,20 +1,27 @@
-from asyncio import get_event_loop
-
 from pony.orm import db_session, select, exists
-from telebot.types import Message
 
 from room_access.entities import Admin
 
+
 @db_session
-def check_admin_username_exist(username: str) -> bool:
+def get_admin_by_id(admin_id: int) -> Admin:
+    """Возваращет сущность администратора по его ID"""
+    return Admin.get(id=admin_id)
+
+@db_session
+def get_admin_by_username(username: str) -> Admin:
+    """Возвращает сущность администратора по имени пользователя в телеграм"""
+    return Admin.get(telegram_username=username)
+
+@db_session
+def check_admin_exist_by_username(username: str) -> bool:
     """Проверяет, существует ли администратор с указанным именем пользователя"""
-    return Admin.get(telegram_username=username) is not None
+    return get_admin_by_username(username) is not None
 
 
 if __name__ == '__main__':
     from room_access.entities import db
 
-    db.bind(provider='sqlite', filename='C:/Users/markerviktor/Desktop/tpu_room_access_via_telegram/db.sqlite')
+    db.bind(provider='sqlite',
+            filename='C:/Users/markerviktor/Desktop/tpu_room_access_via_telegram/db.sqlite')
     db.generate_mapping()
-
-    print(check_admin_username_exist('MarkerViktor'))
