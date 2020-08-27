ab41638fbbf83f4c24da7ab87e93e8bf08191e8a
markerviktor <MarkerViktor@outlook.com>
2020-08-27 12:11:04 +0600

    Изменен декоратор admin_required, добавлена функция prepare_command_args

--- room_access/config.py
+++ room_access/config.py
@@ -3,5 +3,3 @@ import os
 DB_PATH = os.environ.get('DB_PATH')
 LOG_PATH = os.environ.get('LOG_PATH')
 TG_TOKEN = os.environ.get('TG_TOKEN')
-
-
--- room_access/utils.py
+++ room_access/utils.py
@@ -1,13 +1,18 @@
+import json
+import logging
+from typing import Tuple, List
+
 from telebot.types import Message
 
 from room_access.app import bot
-from room_access.services.admin_sevice import check_admin_username_exist
+from room_access.services.admin_sevice import check_admin_exist_by_username
 
 
 def admin_required(controller):
     """Декоратор. Проверка на то, что сообщение от администратора"""
     def wrapper(message: Message):
-        if check_admin_username_exist(message.from_user.username):
+        admin_username = message.from_user.username
+        if check_admin_exist_by_username(admin_username):
             return controller(message)
         else:
             bot.send_message(
@@ -15,3 +20,28 @@ def admin_required(controller):
                 text='❌ Вы не являетесь администратором, доступ запрещен!'
             )
     return wrapper
+
+
+# Не закончено
+def logger(func):
+    """Декоратор. Ловит все исключения, логирует их и опять выбрасывает исключение."""
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            raise e
+
+
+def prepare_command_args(message_text: str) -> tuple:
+    """Принимает строку, содержащую текст сообщения c командой,
+    возвращает кортеж со всеми аргументами, разделенными пробелами"""
+    words: List[str] = message_text.split(' ')
+    words.pop(0)
+
+    args = []
+    for word in words:
+        try:
+            args.append(int(word))
+        except ValueError:
+            args.append(word)
+    return tuple(args)
