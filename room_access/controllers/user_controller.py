5b4319df250c6b8261aa54cfdd2ebec38ebd44a3
markerviktor <MarkerViktor@outlook.com>
2020-08-27 12:07:43 +0600

    Добавлен обработчик для команды /new_user и необходимые для его реализации функции в сервисе пользователя

--- room_access/controllers/user_controller.py
+++ room_access/controllers/user_controller.py
@@ -2,7 +2,7 @@ from telebot import types
 
 from room_access.app import bot
 from room_access.services import user_service
-from room_access.utils import admin_required
+from room_access.utils import admin_required, prepare_command_args
 
 
 @bot.message_handler(commands=['users_list'])
@@ -12,12 +12,36 @@ def users_list(message: types.Message):
     Выполняется при получении команды /users_list .
     Отвечает сообщением со списком всех пользователей и их ID
     """
-    users = user_service.get_all_users_short_info()
+    users: tuple = user_service.users_list()
 
-    answer_string = f"*В системе зарегистрировано пользователей — {len(users)}:*\n" \
+    answer_string = f"*Всего пользователей — {len(users)}:*\n" \
                     "`\{user\_id\} : \{last\_name\} \{first\_name\}`\n"
 
     for user in users:
         answer_string += f'{user.id} : {user.last_name} {user.first_name}\n'
 
     bot.send_message(chat_id=message.chat.id, text=answer_string, parse_mode='MarkdownV2')
+
+
+@bot.message_handler(commands=['new_user'])
+@admin_required
+def new_user(message: types.Message):
+    """Создает нового пользователя"""
+    try:
+        user_service.new_user(command_string=message.text)
+        answer_text = 'Пользователь успешно создан.'
+    except user_service.UserAlreadyExist:
+        answer_text = 'Пользователь с заданным очетанием имени и фамилии уже существует!'
+    except user_service.BadUserCreatingParamsTypes:
+        answer_text = 'Фамилия или имя не могут состоять только из цифр!'
+    except user_service.BadUserCreatingParams:
+        answer_text = 'Неверный формат параметров команды создания пользователя!'
+
+    bot.send_message(chat_id=message.chat.id, text=answer_text)
+
+
+@bot.message_handler(commands=['delete_user'])
+@admin_required
+def delete_user(message: types.Message):
+    """Удаляет пользователя по ID"""
+    pass
--- room_access/services/user_service.py
+++ room_access/services/user_service.py
@@ -1,15 +1,56 @@
 from collections import namedtuple
-from asyncio import get_event_loop
+from typing import Tuple
 
 from pony.orm import select, db_session
-from pony.orm.core import QueryResult
 
 from room_access.entities import User
+from room_access.utils import prepare_command_args
 
-UserShortInfo = namedtuple('UserShortInfo', ('id', 'first_name', 'last_name'))
+
+class UserAlreadyExist(Exception):
+    """Попытка создать пользователя, который уже существует"""
+    pass
+
+class BadUserCreatingParams(Exception):
+    """Проблема с параметрами запроса создания пользователя"""
+    pass
+
+class BadUserCreatingParamsTypes(BadUserCreatingParams):
+    """Проблема с типом параметров запроса создания пользователя"""
+    pass
+
 
 @db_session
-def get_all_users_short_info():
+def create_user_entity(first_name: str, last_name: str) -> User:
+    """Добавляет в БД нового пользователя"""
+    old_user = User.get(first_name=first_name, last_name=last_name)
+    if old_user is None:
+        return User(first_name=first_name, last_name=last_name)
+    else:
+        raise UserAlreadyExist(f'User with such combination of first and last names'
+                               f' ({last_name} {first_name}) is already exist!')
+
+def new_user(command_string: str) -> None:
+    """Создание нового пользователя"""
+    args: tuple = prepare_command_args(command_string)
+
+    try:
+        create_user_entity(first_name=args[1], last_name=args[0])
+    except TypeError:
+        raise BadUserCreatingParamsTypes
+    except IndexError:
+        raise BadUserCreatingParams
+
+
+def delete_user(command_string: str):
+    args: tuple = prepare_command_args(command_string)
+
+
+UserShortInfo = namedtuple(typename='UserShortInfo',
+                           field_names=('id', 'first_name', 'last_name'))
+
+@db_session
+def users_list() -> Tuple[UserShortInfo]:
     """
     Получение информации обо всех пользователях
     в виде namedtuple с атрибутами id, first_name, last_name
@@ -23,5 +64,3 @@ if __name__ == '__main__':
 
     db.bind(provider='sqlite', filename='C:/Users/markerviktor/Desktop/tpu_room_access_via_telegram/db.sqlite')
     db.generate_mapping()
-
-
